4a34f3f62cf75c2b5f7444e354c23fa5e428b269
Andreas Falk <andreas.falk@novatec-gmbh.de>
2016-08-22 00:10:35 +0200

    add csrf protection

--- addressbook-server/src/main/java/com/example/addressbook/config/WebSecurityConfiguration.java
+++ addressbook-server/src/main/java/com/example/addressbook/config/WebSecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.addressbook.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,18 +23,28 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+//            .csrf().disable()
             .httpBasic()
             .and()
             .cors()
             .and()
             .authorizeRequests().anyRequest().authenticated();
 
-/*
+
         http
             .csrf()
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-*/
+
+    }
+
+    /**
+     * Configurer for CORS filter in OAuth2 resource server.
+     *
+     * @return the {@link ResourceServerConfigurer}
+     */
+    @Bean
+    public ResourceServerConfigurer resourceServerConfigurer(ResourceServerProperties resourceServerProperties) {
+        return new CorsResourceServerConfigurer(resourceServerProperties);
     }
 
     @Bean
@@ -43,5 +57,38 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    /**
+     * Configures CORS filter for OAuth2 resource server.
+     */
+    public static class CorsResourceServerConfigurer implements ResourceServerConfigurer {
+
+        private final ResourceServerProperties resourceServerProperties;
+
+        /**
+         * Constructor.
+         *
+         * @param resourceServerProperties properties for resource server
+         */
+        public CorsResourceServerConfigurer(ResourceServerProperties resourceServerProperties) {
+            this.resourceServerProperties = resourceServerProperties;
+        }
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.resourceId(resourceServerProperties.getResourceId());
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .cors()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .and()
+                    .authorizeRequests().anyRequest().authenticated();
+        }
     }
 }
---
+++ addressbook-server/src/main/java/com/example/addressbook/user/api/UserController.java
@@ -0,0 +1,7 @@
+package com.example.addressbook.user.api;
+
+/**
+ * Created by afa on 18.08.16.
+ */
+public class UserController {
+}
